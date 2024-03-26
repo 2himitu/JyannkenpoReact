04208f7f7fa7de55b86521ebc45685ea45cf531e
SeonSo <101612857+SeonSo@users.noreply.github.com>
2024-03-26 16:17:10 +0900

    웹캠테스트

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Style/App';
+import Tensorflow from './Tensorflow';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Tensorflow />
   </React.StrictMode>
 );
